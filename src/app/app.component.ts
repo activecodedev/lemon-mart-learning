4f3e2bc6d629829a2b321580ff06792723626c9a
activecodedev <rafal.pachnicki@outlook.com>
2024-10-01 12:23:16 +0200

    Change angular material to primeng and primeflex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core'
 import { RouterLink, RouterOutlet } from '@angular/router'
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { ButtonModule } from 'primeng/button'
+import { ToolbarModule } from 'primeng/toolbar'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule],
+  imports: [RouterOutlet, RouterLink, ToolbarModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'lemon-mart-learning'
+
+  // constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  //   iconRegistry.addSvgIcon(
+  //     'store',
+  //     sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/store.svg')
+  //   )
+  // }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core'
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: ` <p>home works!</p> `,
-  styles: ``,
+  imports: [ButtonModule],
+  template: `
+    <div
+      class="flex flex-column align-content-center align-items-center justify-content-center border-primary-500 border-round m-2"
+    >
+      <p class="text-5xl">Hello, Liumoncu!</p>
+      <button pButton pRipple class="p-button-success">Login</button>
+    </div>
+  `,
+  styles: [
+    `
+      div {
+        margin-top: 32px;
+      }
+    `,
+  ],
 })
 export class HomeComponent {}
